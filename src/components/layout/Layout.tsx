f52b6f7f5fc05a4d091cfa8808d29da73c058050
jcl <lamypro66@gmail.com>
2025-09-17 20:49:22 +0200

    git init

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,15 @@
+import { Footer } from "./Footer";
+import { Header } from "./Header";
+import { Outlet } from "react-router-dom";
+
+export const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
